be556e314cad2ad4d74d197c4ecc7203264ad7b0
mxd1365 <mike.dapiran@gmail.com>
2019-05-25 15:25:32 -0400

    Added CharacterIF

---
+++ Project G Fox/Assets/Scripts/CharacterIF.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface CharacterIF : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int curHealth = 100;
+
+    public int TakeDamage(int damage);
+    public void Attack(int row);
+    
+
+}
