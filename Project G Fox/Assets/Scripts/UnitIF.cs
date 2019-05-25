9134cd7c5d016d4d41d97439c128cec1af5dc364
benbuchar <benbuchar@gmail.com>
2019-05-25 18:27:39 -0400

    Unit grid updates
    ability to modify character locations in grid

---
+++ Project G Fox/Assets/Scripts/UnitIF.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface UnitIF
+{
+    bool AddCharacterToGrid(CharacterIF c, int x, int y);
+    bool MoveCharacterInGrid(CharacterIF c, int x, int y);
+    (int, int) FindCharacterInGrid(CharacterIF c);
+}
