ce9e81d80ef93003ecfe1d4912085029343e2b9b
mxd1365 <mike.dapiran@gmail.com>
2019-05-25 15:29:22 -0400

    Character stuff

--- Project G Fox/Assets/Scripts/CharacterIF.cs
+++ Project G Fox/Assets/Scripts/CharacterIF.cs
@@ -6,6 +6,8 @@ public interface CharacterIF : MonoBehaviour
 {
     public int maxHealth = 100;
     public int curHealth = 100;
+    public int strength = 10;
+    public int defense = 5;
 
     public int TakeDamage(int damage);
     public void Attack(int row);
