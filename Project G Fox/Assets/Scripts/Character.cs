99b2dd8e3dee29955a0424b14ab51b0ecccb1747
mxd1365 <mike.dapiran@gmail.com>
2019-05-25 16:02:34 -0400

    Character stuff

--- Project G Fox/Assets/Scripts/Character.cs
+++ Project G Fox/Assets/Scripts/Character.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Character : MonoBehaviour, CharacterIF
 {
+    public int maxHealth = 100;
+    public int currentHealth = 100;
+    public int strength = 10;
+    public int defense = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public int TakeDamage(int damage)
+    {
+        int damageTaken = Math.Max(damage - defense, 0);
+        currentHealth -= damageTaken;
+        return damageTaken;
+    }
+
+    public void Attack(CharacterIF target, int row)
+    {
+        target.TakeDamage(strength);
     }
 }
--- Project G Fox/Assets/Scripts/CharacterIF.cs
+++ Project G Fox/Assets/Scripts/CharacterIF.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface CharacterIF : MonoBehaviour
+public interface CharacterIF
 {
-    public int maxHealth = 100;
-    public int curHealth = 100;
-    public int strength = 10;
-    public int defense = 5;
-
-    public int TakeDamage(int damage);
-    public void Attack(int row);
+    int TakeDamage(int damage);
+    void Attack(CharacterIF target, int row);
     
 
 }
